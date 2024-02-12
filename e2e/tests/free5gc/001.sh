e7c5d13309cc46a9c589ced2ceb468a1ed2e3c01
efiacor <fiachra.corcoran@est.tech>
2024-02-12 09:48:09 +0000

    Remove log assertions from 001.sh

--- e2e/tests/free5gc/001.sh
+++ e2e/tests/free5gc/001.sh
@@ -35,48 +35,25 @@ source "${LIBDIR}/_assertions.sh"
 # shellcheck source=e2e/lib/_utils.sh
 source "${LIBDIR}/_utils.sh"
 
-function _curl_workload_cluster_content {
-    curl -s "http://$(kubectl get services -n gitea gitea -o jsonpath='{.status.loadBalancer.ingress[0].ip}'):3000/nephio/mgmt/raw/branch/$1/regional/workload-cluster.yaml"
-}
-
-# Assertions
-
-function assert_workload_resource_contains {
-    assert_contains "$(_curl_workload_cluster_content "$1")" "$2" "$3"
-}
 
 regional_pkg_rev=$(porchctl rpkg clone -n default "https://github.com/nephio-project/catalog.git/infra/capi/nephio-workload-cluster@$REVISION" --repository mgmt regional | cut -f 1 -d ' ')
 k8s_wait_exists "packagerev" "$regional_pkg_rev"
 
-# Draft creation
-porch_wait_log_entry "Creating packagerev default/mgmt-"
-assert_lifecycle_equals "$regional_pkg_rev" "Draft"
-assert_branch_exists "drafts/regional/v1"
-assert_commit_msg_in_branch "Intermediate commit" "drafts/regional/v1"
-assert_workload_resource_contains "drafts/regional/v1" "clusterName: regional" "Workload cluster has not been transformed properly"
 
 pushd "$(mktemp -d -t "001-pkg-XXX")" >/dev/null
 trap popd EXIT
+
 porchctl rpkg pull -n default "$regional_pkg_rev" regional
 kpt fn eval --image "gcr.io/kpt-fn/set-labels:v0.2.0" regional -- "nephio.org/site-type=regional" "nephio.org/region=us-west1"
 assert_contains "$(cat regional/workload-cluster.yaml)" "nephio.org/region: us-west1" "Workload cluster doesn't have region label"
 
 porchctl rpkg push -n default "$regional_pkg_rev" regional
-porch_wait_log_entry "Update.*packagerevisionresources/${regional_pkg_rev},"
 
 # Proposal
 porchctl rpkg propose -n default "$regional_pkg_rev"
-porch_wait_log_entry "Update.*packagerevisions/${regional_pkg_rev},"
-assert_lifecycle_equals "$regional_pkg_rev" "Proposed"
-assert_branch_exists "proposed/regional/v1"
-assert_commit_msg_in_branch "Intermediate commit" "proposed/regional/v1"
-assert_workload_resource_contains "proposed/regional/v1" "nephio.org/site-type: regional" "Workload cluster has not been transformed properly to proposed"
 
 # Approval
 porchctl rpkg approve -n default "$regional_pkg_rev"
-porch_wait_log_entry "Update.*/${regional_pkg_rev}.*/approval"
-assert_lifecycle_equals "$regional_pkg_rev" "Published"
-assert_workload_resource_contains "main" "nephio.org/site-type: regional" "Workload cluster has not been successfully merged into main branch"
 
 k8s_wait_exists "workloadcluster" "regional"
 capi_cluster_ready "regional"
